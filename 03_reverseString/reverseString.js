1154f6a4b2c0aef1c640ba5898a4e270c6e6ffcf
Christian Esparza <cesparzajp@gmail.com>
2024-03-03 14:45:10 +0900

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    reverseText = "";
+    textLength = text.length;
+    for(let i=1; i <= textLength; i++){
+        reverseText += text.charAt(textLength-i);
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
