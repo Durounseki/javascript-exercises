03e2cb2ee6a160423f62263bdd5a7e8b626ef15f
Christian Esparza <cesparzajp@gmail.com>
2024-03-03 15:02:59 +0900

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    let element = undefined;
+    let index = -1;
+    for(let i=0; i<elements.length; i++){
+        element=elements[i];
+        index = array.indexOf(element);
+        while(index >=0 ){
+            array.splice(index,1);
+            index = array.indexOf(element);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
