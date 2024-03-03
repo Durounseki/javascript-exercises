466a1b4e18753006506ded4751735085578035aa
Christian Esparza <cesparzajp@gmail.com>
2024-03-03 14:31:26 +0900

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, numRepeat) {
+    let repeatText = ""
+    if (numRepeat >= 0){
+        for(let i=0; i<numRepeat; i++){
+            repeatText += text;
+        }
+        return repeatText;
+    }else{
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
