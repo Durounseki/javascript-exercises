57f2c847765c12b11a448ffd67f5efedad858cab
Christian Esparza <cesparzajp@gmail.com>
2024-03-04 16:51:55 +0900

    Solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let quotient = year/4
+    if(quotient !== Math.floor(quotient)){
+        return false;
+    }
+    if( quotient%25 === 0){
+        if(quotient%4 !== 0){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
