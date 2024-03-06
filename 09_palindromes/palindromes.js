fb3f53bd0ed6e52aa146f99f5fc7975676b25049
Christian Esparza <cesparzajp@gmail.com>
2024-03-06 16:33:21 +0900

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    //Remove symbols and capitalize
+    letters = word.replace(/[^\w]/gi,'').toUpperCase(); //Numbers unaffected
+    let reverseWord = '';
+    let len = letters.length;
+    //reverse characters
+    for(let i = 1; i <= len; i++){
+        reverseWord += letters.charAt(len-i);
+    }
+    return letters === reverseWord;
 };
 
 // Do not edit below this line
